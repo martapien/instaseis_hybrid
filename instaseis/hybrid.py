Correct traction computation in generate_output

instaseis/hybrid.py
@@ -14,9 +14,11 @@ from __future__ import (absolute_import, division, print_function,
 
 import numpy as np
 import h5py
+from math import cos, sin
+from scipy.integrate import cumtrapz
+
 from . import rotations
 from .source import (Source, ForceSource, Receiver)
-from scipy.integrate import cumtrapz
 
 
 def hybrid_generate_output(outputfile, inputfile, source, database, dt,
@@ -88,6 +90,11 @@ def hybrid_generate_output(outputfile, inputfile, source, database, dt,
             normals = f_in['local/normals'][:, :]
             rotmat = f_in['local'].attrs['rotation-matrix']
             normals = np.dot(normals, rotmat)
+        mu_all = f_in['elastic_params/mu']
+        lbd_all = f_in['elastic_params/lambda']
+        xi_all = f_in['elastic_params/xi']
+        phi_all = f_in['elastic_params/phi']
+        eta_all = f_in['elastic_params/eta']
 
     npoints = len(receivers)
     ntimesteps = _get_ntimesteps(database, source, receivers[0], dt,
@@ -141,15 +148,68 @@ def hybrid_generate_output(outputfile, inputfile, source, database, dt,
             dset_strn[i, :, :] = data["strain"]
         if "traction" in dumpfields:
             strain = data["strain"]
+            e_tt = np.array(strain[:, 0])
+            e_pp = np.array(strain[:, 1])
+            e_rr = np.array(strain[:, 2])
+            e_rp = np.array(strain[:, 3])
+            e_rt = np.array(strain[:, 4])
+            e_tp = np.array(strain[:, 5])
+
             n = normals[i, :]
             traction = np.zeros(ntimesteps, 3)
-            # review is it correct: compute traction
-            traction[:, 0] = strain[:, 0] * n[0] + strain[:, 3] * n[1] + \
-                             strain[:, 4] * n[2]
-            traction[:, 1] = strain[:, 3] * n[0] + strain[:, 1] * n[1] + \
-                             strain[:, 5] * n[2]
-            traction[:, 2] = strain[:, 4] * n[0] + strain[:, 5] * n[1] + \
-                             strain[:, 2] * n[2]
+
+            mu = mu_all[i]
+            lbd = lbd_all[i]
+            xi = xi_all[i]
+            phi = phi_all[i]
+            eta = eta_all[i]
+            fa_ani_thetal = 0.0
+            fa_ani_phil = 0.0
+
+            c_11 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 0, 0, 0, 0)
+            c_12 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 0, 0, 1, 1)
+            c_13 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 0, 0, 2, 2)
+            c_15 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 0, 0, 2, 0)
+            c_22 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 1, 1, 1, 1)
+            c_23 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 1, 1, 2, 2)
+            c_25 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 1, 1, 2, 0)
+            c_33 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 2, 2, 2, 2)
+            c_35 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 2, 2, 2, 0)
+            c_44 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 1, 2, 1, 2)
+            c_46 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 1, 2, 0, 1)
+            c_55 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 2, 0, 2, 0)
+            c_66 = _c_ijkl_ani(lbd, mu, xi, phi, eta, fa_ani_thetal,
+                                    fa_ani_phil, 0, 1, 0, 1)
+
+            traction[:, 0] = n[0] * (c_11 * e_tt + 2.0 * c_15 * e_rt +
+                                     c_12 * e_pp + c_13 * e_rr) + \
+                             n[1] * 2.0 * (c_66 * e_tp + c_46 * e_rp) + \
+                             n[2] * (c_15 * e_tt + c_25 * e_pp + c_35 * e_rr
+                                     + 2.0 * c_55 * e_rt)
+
+            traction[:, 1] = n[0] * 2.0 * (c_66 * e_tp + c_46 * e_rp) + \
+                             n[1] * (c_12 * e_tt + 2.0 * c_25 * e_rt +
+                                     c_22 * e_pp + c_23 * e_rr) + \
+                             n[2] * 2.0 * (c_46 * e_tp + c_44 * e_rp)
+
+            traction[:, 2] = n[0] * (c_15 * e_tt + 2.0 * c_55 * e_rt +
+                                     c_25 * e_pp + c_35 * e_rr) + \
+                             n[1] * 2.0 * (c_46 * e_tp + c_44 * e_rp) + \
+                             n[2] * (c_13 * e_tt + 2.0 * c_35 * e_rt +
+                                     c_23 * e_pp + c_33 * e_rr)
+
             dset_trac[i, :, :] = traction
 
     f_out.close()
@@ -282,6 +342,46 @@ def _get_ntimesteps(database, source, receiver, dt, filter_freqs):
     disp = data["displacement"][:, 0]
 
     return len(disp)
+
+
+# review rethink structure as this function is defined in source.py (HybridS.)
+def _c_ijkl_ani(lbd, mu, xi_ani, phi_ani, eta_ani, theta_fa, phi_fa,
+                i, j, k, l):
+
+    deltaf = np.zeros([3,3])
+    deltaf[0, 0] = 1.
+    deltaf[1, 1] = 1
+    deltaf[2, 2] = 1
+
+    s = np.zeros(3)  # for transverse anisotropy
+    s[0] = cos(phi_fa) * sin(theta_fa)  # 0.0
+    s[1] = sin(phi_fa) * sin(theta_fa)  # 0.0
+    s[2] = cos(theta_fa)  # 1.0
+
+    c_ijkl_ani = 0.0
+
+    # isotropic part:
+    c_ijkl_ani += lbd * deltaf[i, j] * deltaf[k, l]
+
+    c_ijkl_ani += mu * (deltaf[i, k] * deltaf[j, l]
+                        + deltaf[i, l] * deltaf[j, k])
+
+    # anisotropic part in xi, phi, eta
+    c_ijkl_ani += ((eta_ani - 1.0) * lbd + 2.0 * eta_ani * mu *
+                   (1.0 - 1.0 / xi_ani)) * (deltaf[i, j] * s[k] * s[l]
+                                            + deltaf[k, l] * s[i] * s[j])
+
+    c_ijkl_ani += mu * (1.0 / xi_ani - 1.0) *\
+                  (deltaf[i, k] * s[j] * s[l]
+                   + deltaf[i, l] * s[j] * s[k]
+                   + deltaf[j, k] * s[i] * s[l]
+                   + deltaf[j, l] * s[i] * s[k])
+
+    c_ijkl_ani += ((1.0 - 2.0 * eta_ani + phi_ani) * (lbd + 2.0 * mu)
+                   + (4. * eta_ani - 4.) * mu / xi_ani)\
+                   * (s[i] * s[j] * s[k] * s[l])
+
+    return c_ijkl_ani
 
 
 class HybridSourcesInternalTest(object):

instaseis/source.py
@@ -1735,13 +1735,6 @@ class HybridSources(object):
                      n[2] * (c_13 * e_tt + 2.0 * c_35 * e_rt + c_23 * e_pp +
                              c_33 * e_rr)
 
-                """
-                t2 = 2.0 * n[0] * mu * np.array(strain[:, 4]) \
-                    + 2.0 * n[1] * mu * np.array(strain[:, 3]) \
-                    + n[2] * lbd * np.array(strain[:, 0]) \
-                    + n[2] * lbd * np.array(strain[:, 1]) \
-                    + n[2] * (lbd + 2.0 * mu) * np.array(strain[:, 2])
-                """
             pointsources.append(ForceSource(latitude, longitude,
                                             depth_in_m=depth_in_m,
                                             f_r=0, f_t=w, f_p=0,
@@ -1757,7 +1750,7 @@ class HybridSources(object):
 
         return pointsources
 
-    def _c_ijkl_ani(self, lbd , mu, xi_ani, phi_ani, eta_ani,
+    def _c_ijkl_ani(self, lbd, mu, xi_ani, phi_ani, eta_ani,
                    theta_fa, phi_fa, i, j, k, l):
 
         deltaf = np.zeros([3,3])
@@ -1765,7 +1758,7 @@ class HybridSources(object):
         deltaf[1, 1] = 1
         deltaf[2, 2] = 1
 
-        s = np.zeros(3) #  for transverse anisotropy
+        s = np.zeros(3)  # for transverse anisotropy
         s[0] = cos(phi_fa) * sin(theta_fa)  # 0.0
         s[1] = sin(phi_fa) * sin(theta_fa)  # 0.0
         s[2] = cos(theta_fa)  # 1.0
@@ -1777,7 +1770,6 @@ class HybridSources(object):
 
         c_ijkl_ani += mu * (deltaf[i, k] * deltaf[j, l]
                             + deltaf[i, l] * deltaf[j, k])
-
 
         # anisotropic part in xi, phi, eta
         c_ijkl_ani += ((eta_ani - 1.0) * lbd + 2.0 * eta_ani * mu *